c5456f33d9d896929a8ad951f052860471dfdeff
YuvalGal2 <muonline.mega@gmail.com>
2019-08-02 23:52:28 +0300

    Work in progress, created basic for loop to go over the board, need to move to functions

--- src/app/shared/board.service.ts
+++ src/app/shared/board.service.ts
@@ -1,71 +1,114 @@
 import { Queen } from './models/queen.model';
 import { Injectable } from '@angular/core';
+import { isFulfilled } from 'q';
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class BoardService {
-  private numOfRowsAndCols;
-  private gameStatus = false;
-  private queensPositions:Queen["position"][] = [];
+	private numOfRowsAndCols;
+	private gameStatus = false;
+	private queensPositions: Queen["position"][] = [];
 
 
-  constructor() { }
-  
-  validateQueenPosition(queenPosition:Queen["position"]):boolean {
-    const forbiddenCells = [];
-
-    // TODO: 
-
-      //  foreach queen placed,
-      // go from top left  -> one col bottom right
-          let row = queenPosition.row;
-          for(let col = queenPosition.col+1 ; col <= this.numOfRowsAndCols;col++) {
-            forbiddenCells.push([++row,col])
-          }
-        
+	constructor() { }
 
 
+	checkDiagonalLineLtr(queenPosition: Queen["position"]):any {
+		return false;
+	}
+	checkDiagonalLineRtl(queenPosition: Queen["position"]):any {
+		return false;
+	}
+	checkTopToBottom(queenPosition: Queen["position"]):any {
+		return false;
+	}
+	checkLeftToRight(queenPosition: Queen["position"]):any {
+		return false;
+	}
+	
+	validateQueenPosition(queenPosition: Queen["position"]): boolean {
+		const forbiddenCells = [];
+		console.log(`clicked on: `);
+		console.log(queenPosition);
+		console.log('-----');
+		// TODO:
+		// * the condition: if cell isnt in the array 
+		// * after condition is passed.
 
-      // go from top right  -> one col bottom left
+		// TODO: 
+		//forbiddenCells.push([++row,col]) -- if the condition is passed.
 
-      // go from left to right
+		//  foreach queen placed,
+		// go from top left  -> one col bottom right
 
-      // go from top to bottom
-
-      // and map the forbiddan places. 
-      // then match it with the global forbiddan places array with the map opreator.
-      // if there is no match, meaning there won't be a problem placing a queen there.
+		for (let row = queenPosition.row, col = queenPosition.col + 1; col <= this.numOfRowsAndCols; col++) {
+			forbiddenCells.push([++row, col,'diagonal line left to right'])
+		}
 
 
-      // pass the forbiddan array to the global one and push the queen.
-      // very important to also pass on the the global forbiddan the location of the newly 
-      // placed queen.
+		// go from top right  -> one col bottom left
+		if (queenPosition.col - 1 > 0 && queenPosition.row - 1 > 0) {
+			for (let row = queenPosition.row, col = queenPosition.col - 1; col > 0; col--) {
+				if (--row > 0 && col > 0) {
+					forbiddenCells.push([row, col,'diagonal line right to to left'])
+				}
 
-      // update the view
-      // return false is queen is killed.
-    
-   // console.log(forbiddenCells);
-    return false;
-  } 
+			}
+		}
 
-  setQueenPosition(queenPosition:Queen['position']):void { 
-    this.queensPositions.push(queenPosition);
-    this.validateQueenPosition(queenPosition);
-  }
+		// go from left to right
+		for (let col = 1; col <= this.numOfRowsAndCols; col++) {
+			if (col !== queenPosition.col) {
+				forbiddenCells.push([queenPosition.row, col,'left to right'])
+			}
+		}
 
-  getGameStatus():boolean {
-    return this.gameStatus;
-  }
+		// go from top to bottom
+		for (let row = 1; row <= this.numOfRowsAndCols; row++) {
+			if (row !== queenPosition.row) {
+				forbiddenCells.push([row, queenPosition.col,'top to bottom'])
+			}
+		}
+		console.log(forbiddenCells);
 
-  setGameStatus(newGameStatus:boolean):boolean { 
-    return this.gameStatus = newGameStatus;
-  }
-  
-  setBoardSize(size:number = 8):void {
-    this.numOfRowsAndCols = size;
-  }
-  
-  getBoardSize():number { 
-    return this.numOfRowsAndCols;
-  }
+		// and map the forbiddan places. 
+		// then match it with the global forbiddan places array with the map opreator.
+		// if there is no match, meaning there won't be a problem placing a queen there.
+
+
+		// pass the forbiddan array to the global one and push the queen.
+		// very important to also pass on the the global forbiddan the location of the newly 
+		// placed queen.
+
+		// update the view
+		// return false is queen is killed.
+
+		// console.log(forbiddenCells);
+
+		// TODO: 
+		// move logic to spearte function set and create simple if statement here.
+		return false;
+
+	}
+
+setQueenPosition(queenPosition: Queen['position']): void {
+	this.queensPositions.push(queenPosition);
+	this.validateQueenPosition(queenPosition);
+}
+
+getGameStatus(): boolean {
+	return this.gameStatus;
+}
+
+setGameStatus(newGameStatus: boolean): boolean {
+	return this.gameStatus = newGameStatus;
+}
+
+setBoardSize(size: number = 8): void {
+	this.numOfRowsAndCols = size;
+}
+
+getBoardSize(): number {
+	return this.numOfRowsAndCols;
+}
 }
